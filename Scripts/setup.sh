84f1d5420e24cc3c5e74034a4c6ac071bace29f7
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2021-05-11 11:33:25 +0800

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-OPENSSL_CONF=/etc/ssl/
+export OPENSSL_CONF=/etc/ssl/
 pip install rpa
 pip install retirejs
+
+#Setup chrome for RPA
 wget https://www.google.com/chrome/thank-you.html?platform=linux&statcb=0&installdataindex=empty&defaultbrowser=0# -O chrome.deb
 apt install -y chrome.deb
+sed -i '$ s/$/ --no-sandbox/' /usr/bin/google-chrome
+
+#Setup services for DVWA
+apt install -y apache2
+systemctl start apache2
+systemctl start mysql
+systemctl enable apache2
+systemctl enable mysql
+
+#Setup DVWA
+cd /var/www/html
+git clone https://github.com/digininja/DVWA.git
