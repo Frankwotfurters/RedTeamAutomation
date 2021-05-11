a7ea3e900560234dcef585fe8dc1c7b0f7f814ec
JustLikeChuu <ethanngsq@gmail.com>
2021-05-11 11:50:58 +0800

    updated GUI

--- Scripts/app.py
+++ Scripts/app.py
@@ -3,17 +3,49 @@ import csrf
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
 	return render_template("index.html", content=["joe", "mama", "hortons"])
 
-@app.route("/<name>")
-def user(name):
-	return f"Hello {name}"
+@app.route("/sqli")
+def sqliPage():
+	return "SQLI"
+
+@app.route("/subdomain")
+def subdPage():
+	return "subdomain"
+
+@app.route("/th-subdomain")
+def thsubdPage():
+	return "threaded-subdomain"
+
+@app.route("/admin-scanner")
+def adminScannerPage():
+	return "Admin Scanner"
+
+@app.route("/clickjack")
+def clickjackPage():
+	return "Clickjack"
+
+@app.route("/xss")
+def xssPage():
+	return "XSS"
+
+@app.route("/sensitive-data")
+def sendataPage():
+	return "Sensitive Data Exposure"
+
+@app.route("/link-extract")
+def linkextractPage():
+	return "Link Extractor"
 
 @app.route("/csrf")
 def csrfPage():
-	return csrf.main()
+	return csrf.main
+
+@app.route("/vuln-components")
+def vulncompPage():
+	return "Vulnerable Components"
 
 if  __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
