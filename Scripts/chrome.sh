3eb1a7b2576006f9e37083da2dabbbcb2f48449f
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2021-05-27 05:09:54 +0800

    cleanup & set $openssl_conf

--- Scripts/chrome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
-rm -f ./google-chrome-stable_current_amd64.deb
