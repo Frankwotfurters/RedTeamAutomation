657a2809bb5abf7481d64252d7de8bf42e4e4708
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2021-05-27 04:37:03 +0800

    setup script done

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+sed -i '$ s/$/ --no-sandbox/' $(readlink -f /usr/bin/google-chrome)
+rm -f ./google-chrome-stable_current_amd64.deb
+clear
+
 #Install python modules
 apt update
 apt install -y pip
@@ -16,11 +23,7 @@ pip install pprint
 pip install colorama
 pip install flask
 export OPENSSL_CONF=/etc/ssl/
-
-#Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
-rm -f ./google-chrome-stable_current_amd64.deb
+clear
 
 #Setup services for DVWA
 apt install -y apache2
@@ -28,10 +31,14 @@ systemctl start apache2
 systemctl start mysql
 systemctl enable apache2
 systemctl enable mysql
+clear
 
 #Setup DVWA
 git clone https://github.com/digininja/DVWA.git /var/www/html/DVWA
+clear
 
 #RPA for Python first time setup
-sed -i '$ s/$/ --no-sandbox/' $(readlink -f /usr/bin/google-chrome)
 python3 rpaSetup.py
+export OPENSSL_CONF=/etc/ssl/
+echo "export OPENSSL_CONF=/etc/ssl/" > /etc/profile.d/openssf_conf
+echo "Setup complete!"
