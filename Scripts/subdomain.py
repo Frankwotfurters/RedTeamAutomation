988bebf0168b6eedb0bc0e35b4f440f10b55d7d0
JustLikeChuu <ethanngsq@gmail.com>
2021-06-05 16:05:25 +0800

    typo

--- Scripts/subdomain.py
+++ Scripts/subdomain.py
@@ -28,7 +28,7 @@ def subdCode(target):
     pdf.set_font('Arial', size=12)
     pdf.cell(200, 10, txt="Scanner: Subdomain Scanner", ln=1, align='L')
     timestart = time.strftime("%d/%m/%Y %I:%M:%S")
-    time1 =  time.strftime("%d-%m-%Y%H%M%S")
+    time1 = time.strftime("%d-%m-%Y%H%M%S")
     pdf.cell(200, 10, txt=f"Scan Time: {timestart}", ln=1, align="L")
     pdf.cell(200, 10, txt="Results: ", ln=1, align='L')
     ss = []
