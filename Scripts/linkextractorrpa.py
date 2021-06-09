85aaa3aaca7626a6c8977e48ed6a04fe821330e4
zhengda28 <64730358+zhengda28@users.noreply.github.com>
2021-06-09 19:55:51 +0800

    link extractor landing page updated 2

--- Scripts/linkextractorrpa.py
+++ Scripts/linkextractorrpa.py
@@ -58,14 +58,20 @@ def scan_link_extract(url, receiver=""):
 
     url_tested = []
     url_totallinks = []
+    url_internallinks = []
+    url_externallinks = []
 
     url_tested.append(url)
     url_totallinks.append(total_len)
+    url_internallinks.append(len(internal_urls))
+    url_externallinks.append(len(external_urls))
 
     results = {}
     results["displayfile"] = displayfile
     results["url_tested"] = url_tested
     results["url_totallinks"] = url_totallinks
+    results["url_internallinks"] = url_internallinks
+    results["url_externallinks"] = url_externallinks
 
     if not receiver == "":
         # Send email
